a49deb31846e8af7d7a75b88a98e33ecd677a51e
Jake Waddle <jwaddle@firebend.com>
2022-08-01 14:30:47 -0500

    adding source call to dfs job

--- main.R
+++ main.R
@@ -1,12 +1,12 @@
 # libraries
 if(!require(pacman)) install.packages('pacman') 
-pacman::p_load(tidyverse, rvest, lubridate, here, glue)
+pacman::p_load(tidyverse, rvest, lubridate, here, glue, furrr, tictoc)
 #pacman::p_load(rvest, lubridate, reshape, tidyverse, future.apply, stringi, jsonlite)
 
 # here for relative path / project
 path <- here::here()
 
 # run dfs scraper
-
+source('./src/get_daily_fantasy_logs.R')
 
 
